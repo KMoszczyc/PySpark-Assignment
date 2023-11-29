bfbc6fe529e3b2a52c0ca93220d094be80a2104a
kmoszczyc <likeyouop@gmail.com>
2023-11-29 20:27:57 +0100

    Clean project structure and imports

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,12 @@
-CLIENTS_DATA_RAW_PATH = 'raw_data/dataset_one.csv'
-DETAILS_DATA_RAW_PATH = 'raw_data/dataset_two.csv'
-OUTPUT_PATH = 'client_data/output.csv'
-LOGS_PATH = 'logs/data_processing.log'
+from pathlib import Path
+import os
+
+ROOT_PATH = Path(__file__).parent.parent
+CLIENTS_DATA_RAW_PATH = os.path.join(ROOT_PATH, 'raw_data/dataset_one.csv')
+DETAILS_DATA_RAW_PATH = os.path.join(ROOT_PATH, 'raw_data/dataset_two.csv')
+OUTPUT_PATH = os.path.join(ROOT_PATH, 'client_data/output.csv')
+LOGS_PATH = os.path.join(ROOT_PATH, 'logs/data_processing.log')
+print('Root path', ROOT_PATH)
+
 LOGGER_NAME = 'Data Processing Log'
+SPARK_APP_NAME = 'DataProcessingApp'
